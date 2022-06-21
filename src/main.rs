90a4c0d2a43505b83e4679b11bf0281879cbffe9
ZaneEme <zaneemerick@gmail.com>
2022-06-21 10:53:49 -0600

    save, basic database setup done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 //! main.rs
 
-use zero_to_prod::run;
 use std::net::TcpListener;
+use zero_to_prod::configuration::get_configuration;
+use zero_to_prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration file");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,11 @@
 //! tests/health_check.rs
 
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-
+use zero_to_prod::{
+    configuration::{get_configuration},
+    startup::run,
+};
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -22,9 +26,67 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = zero_to_prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+
+    let configuration = get_configuration().expect("failed to get configuration");
+
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let client = reqwest::Client::new();
+
+    let body = "name=otto&email=bobotto%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=Otto", "missing email"),
+        ("email=Bobotto@gmail.com", "missing the name"),
+        ("", "missing both email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload sent was {}.",
+            error_message
+        );
+    }
 }
